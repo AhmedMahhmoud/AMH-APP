18656891f05977db73da5f3d782257f01fc1aa8d
AhmedMahhmoud <ahmedmahmoud222622@gmail.com>
2020-11-25 23:18:30 -0800

    start

---
+++ lib/views/HomePage.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<void> _launched;
+  String phone = "";
+  String _launchURL = "https://www.google.com";
+  Future<void> _lunchInBrowser(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url,
+          forceSafariVC: false,
+          forceWebView: false,
+          headers: <String, String>{"header_key": "header_value"});
+    } else {
+      throw "Could not launch $url";
+    }
+  }
+
+  Future<void> _lunchInAPP(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url,
+          forceSafariVC: true,
+          forceWebView: true,
+          headers: <String, String>{"header_key": "header_value"});
+    } else {
+      throw "Could not launch $url";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RaisedButton(
+              child: Text("Launch in browser"),
+              onPressed: () {
+                _lunchInBrowser(_launchURL);
+              },
+            ),
+            RaisedButton(
+              child: Text("Launch in app"),
+              onPressed: () {
+                _lunchInAPP(_launchURL);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
